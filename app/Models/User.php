d2a692ebb6404b94f8304c16ac2c132e1d04396e
lemyskaman <lemyskaman@gmail.com>
2022-08-12 07:16:12 -0400

    quick and dirty

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,24 +4,21 @@ namespace App\Models;
 
 use Laravel\Sanctum\HasApiTokens;
 use App\Models\Scopes\Searchable;
-use App\Models\Traits\FilamentTrait;
 use Spatie\Permission\Traits\HasRoles;
 use Laravel\Jetstream\HasProfilePhoto;
 use Illuminate\Notifications\Notifiable;
-use Filament\Models\Contracts\FilamentUser;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use HasRoles;
     use Notifiable;
     use HasFactory;
     use Searchable;
     use HasApiTokens;
-    use FilamentTrait;
     use HasProfilePhoto;
     use TwoFactorAuthenticatable;
 
